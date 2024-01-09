cb0efdf73e57f12e7a3122f995e36372edd82fdc
Pradyut Das <77787607+prxdyut@users.noreply.github.com>
2024-01-09 17:36:40 +0530

    a

--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -6,43 +6,20 @@ import Signin from "./pages/signin";
 import ImagePreview from "./components/ImagePreview";
 import { useEffect } from "react";
 
-import ReactPWAInstallProvider, { useReactPWAInstall } from "react-pwa-install";
-
 export default function Layout({ children }) {
-  const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
-  const handleClick = () => {
-    pwaInstall({
-      title: "Install Web App",
-      logo: myLogo,
-      features: (
-        <ul>
-          <li>Cool feature 1</li>
-          <li>Cool feature 2</li>
-          <li>Even cooler feature</li>
-          <li>Works offline</li>
-        </ul>
-      ),
-      description: "This is a very good app that does a lot of useful stuff. ",
-    })
-      .then(() =>
-        alert("App installed successfully or instructions for install shown")
-      )
-      .catch(() => alert("User opted out from installing"));
-  };
-
   useEffect(() => {
     const webpushrScript = document.createElement("script");
     webpushrScript.src = "/js/app.js";
     document.querySelector("body").appendChild(webpushrScript);
     const webpushrSubscribe = document.createElement("div");
     webpushrSubscribe.id = "webpushr-subscription-button";
-    webpushrSubscribe.className = "hidden";
+    webpushrSubscribe.className = 'hidden'
     document.querySelector("body").appendChild(webpushrSubscribe);
   }, []);
 
   return (
     <main className=" flex ">
-      {/* <SignedOut>
+      <SignedOut>
         <Signin />
       </SignedOut>
       <SignedIn>
@@ -54,16 +31,7 @@ export default function Layout({ children }) {
           </div>
         </div>
         <ImagePreview />
-      </SignedIn> */}
-      <div>
-        {supported() && !isInstalled() ? (
-          <button type="button" onClick={handleClick}>
-            Install App
-          </button>
-        ) : (
-          <p>PWA uninstallable</p>
-        )}
-      </div>
+      </SignedIn>
     </main>
   );
 }
